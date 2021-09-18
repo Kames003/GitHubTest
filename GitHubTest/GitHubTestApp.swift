3e3855379e974bea6a0a1a9d26d03421e4c5efb7
Kames003 <tomasmucha1314@gmail.com>
2021-09-18 20:43:38 +0200

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Tomáš Mucha on 18/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
